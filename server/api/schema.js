0296b4e9682e737c0a3e81d0841cf82b983d0fab
Pamela Chong <pamelachong@Pamelas-MacBook-Air.local>
2018-07-14 16:30:00 -0700

    Added schema for item, user, tag, file, newiteminput, and mutation

--- server/api/schema.js
+++ server/api/schema.js
@@ -2,44 +2,59 @@ const { gql } = require('apollo-server')
 
 /**
  *  @TODO: Boomtown Schema
- *
- * Define the types in your GraphQL schema here.
- * For each type, remove the `_: Boolean` placeholder and add the
- * fields as directed. Be sure to finish writing resolvers for all types
- * and any relational fields, where required.
- *
- * We will create the custom Date scalar together.
+ * - create custom Date scalar with class
+ * - type Mutation commented out until query completed to prevent server error
  */
 module.exports = gql`
   scalar Upload
-  # scalar Date
+  scalar Date
 
   type Item {
-    _: Boolean
+    id: ID!
+    title: String!
+    imageurl: String
+    description: String!
+    itemowner: User!
+    tags: [Tag]
+    created: Date!
+    borrower: User
   }
 
   type User {
-    _: Boolean
+    id: ID!
+    email: String!
+    fullname: String!
+    bio: String
+    items: [Item]
+    borrowed: [Item]
   }
 
   type Tag {
-    _: Boolean
+    id: ID!
+    title: String!
   }
 
   type File {
-    _: Boolean
+    id: ID!
+    filename: String!
+    mimetype: String!
+    encoding: String!
+    itemid: ID!
   }
 
   input AssignedTag {
-    _: Boolean
+    id: ID!
+    title: String!
   }
 
   input AssignedBorrower {
-    _: Boolean
+    id: ID!
   }
 
   input NewItemInput {
-    _: Boolean
+    title: String!
+    description: String
+    tags: [AssignedTag]
   }
 
   type Query {
@@ -51,6 +66,9 @@ module.exports = gql`
 
   type Mutation {
     addItem: Boolean
+    #  addItem(
+    #     item: NewInputType!
+    #     image: Upload
+    #   ): Item
   }
-
 `
